12cc0eda6367beb19562c8b2b04d36bf66cbf9ca
Varsha567 <varsha.gubbala@gmail.com>
2025-06-28 13:41:28 +0530

    added session again

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const profileRoutes = require('./routes/profileRoutes');
